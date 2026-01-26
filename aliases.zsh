dfdd3bc67b337197287e3001a564dad87dd70302
Maximilien Monteil <maximilienmonteil@gmail.com>
2026-01-26 10:51:29 -0500

    feat: add alias to go to code projects

--- aliases.zsh
+++ aliases.zsh
@@ -11,3 +11,10 @@ alias oclip="xclip -o -sel clipboard"
 # always use neovim
 alias vi=nvim
 alias vim=nvim
+
+# project shortcuts
+alias mylo="cd ~/Code/Mylo"
+alias myapp="cd ~/Code/Mylo/packages/app"
+alias mysite="cd ~/Code/Mylo/packages/site"
+alias blog="cd ~/Code/maxmntl"
+alias jwapp="cd ~/Code/jw-app"
