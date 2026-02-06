5c105b4ac6916d905fdcb17558653212b982e1b4
Maximilien Monteil <maximilienmonteil@gmail.com>
2026-02-06 22:52:32 +0100

    feat: save and restore markdown folds

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,40 +1,41 @@
 --------------------
 -- GLOBAL SETTINGS --
 --------------------
-vim.o.timeoutlen = 300
+vim.o.timeoutlen = 300                  -- How long to wait for next key in combo
 
 ------------
 -- EDITOR --
 ------------
-vim.o.mouse = ''            -- Disable mouse
-vim.o.cursorline = true     -- Show vertical highlight line where cursor is
-vim.o.guicursor = ''        -- Always keep block cursor
-vim.o.number = true         -- Show line numbers
-vim.o.relativenumber = true -- Show relative line numbers
-vim.o.signcolumn = 'yes'    -- Always show the sign column
-vim.o.scrolloff = 1         -- Min number of lines to keep above and below cursor
-vim.o.winborder = 'rounded' -- Rounded outline around popups
-vim.o.splitright = true     -- When splitting vertically, put new window on the right
-vim.o.expandtab = true      -- Uses spaces for tabs
-vim.o.tabstop = 2           -- Number of spaces that a <Tab> is displayed as
-vim.o.softtabstop = 2       -- Number of spaces that a <Tab> writes or deletes
-vim.o.shiftwidth = 0        -- Number of spaces used when indenting (0: use value for 'tabstop')
-vim.o.autoindent = true     -- Copy indent from current line when starting a new line
-vim.o.smartindent = true    -- Do smart autoindenting when starting a new line
-vim.o.smarttab = true       -- Insert <Tab> according to specified widths
-vim.opt.completeopt:append('noselect')
+vim.o.mouse = ''                        -- Disable mouse
+vim.o.cursorline = true                 -- Show vertical highlight line where cursor is
+vim.o.guicursor = ''                    -- Always keep block cursor
+vim.o.number = true                     -- Show line numbers
+vim.o.relativenumber = true             -- Show relative line numbers
+vim.o.signcolumn = 'yes'                -- Always show the sign column
+vim.o.scrolloff = 1                     -- Min number of lines to keep above and below cursor
+vim.o.winborder = 'rounded'             -- Rounded outline around popups
+vim.o.splitright = true                 -- When splitting vertically, put new window on the right
+vim.o.expandtab = true                  -- Uses spaces for tabs
+vim.o.tabstop = 2                       -- Number of spaces that a <Tab> is displayed as
+vim.o.softtabstop = 2                   -- Number of spaces that a <Tab> writes or deletes
+vim.o.shiftwidth = 0                    -- Number of spaces used when indenting (0: use 'tabstop')
+vim.o.autoindent = true                 -- Copy indent from current line when starting a new line
+vim.o.smartindent = true                -- Do smart autoindenting when starting a new line
+vim.o.smarttab = true                   -- Insert <Tab> according to specified widths
+vim.opt.completeopt:append('noselect')  -- Don't auto select the first item
+vim.o.foldmethod = 'manual'             -- Use manual folding, I don't want things autofolded
 
 -- FILE HANDLING --
-vim.o.backup = false        -- Don't create backup files
-vim.o.writebackup = false   -- Don't create backup files before writing
-vim.o.swapfile = false      -- Don't create swapfiles
-vim.o.undofile = true       -- Persistent undo
+vim.o.backup = false                    -- Don't create backup files
+vim.o.writebackup = false               -- Don't create backup files before writing
+vim.o.swapfile = false                  -- Don't create swapfiles
+vim.o.undofile = true                   -- Persistent undo
 
 -- WHITE SPACE --
-vim.o.showbreak = '↪ '      -- Character to show on line breaks
+vim.o.showbreak = '↪ '                  -- Character to show on line breaks
 
-vim.o.list = true           -- Enable white space characters
-vim.opt.listchars = {       -- Custom white space characters
+vim.o.list = true                       -- Enable white space characters
+vim.opt.listchars = {                   -- Custom white space characters
   tab = '▏ ',
   leadmultispace = '▏ ',
   eol = '¬',
@@ -43,6 +44,22 @@ vim.opt.listchars = {       -- Custom white space characters
   nbsp = '·',
   trail = '~',
 }
+
+-- FOLDS --
+-- Save folds in markdown files across sessions
+local folds_augroup = vim.api.nvim_create_augroup('Folds', { clear = true })
+
+vim.api.nvim_create_autocmd({'BufWritePost', 'QuitPre'}, {
+  pattern = '*.md',
+  group = folds_augroup,
+  command = 'mkview',
+})
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*.md',
+  group = folds_augroup,
+  command = 'silent! loadview | normal! zM',
+})
 
 --------------------
 -- SEARCH PATTERN --
