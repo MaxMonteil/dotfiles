0f901b48183b285d541c27c62ce9e3695dbe8194
Maximilien Monteil <maximilienmonteil@gmail.com>
2026-02-08 05:49:11 +0100

    feat(nvim): add lsp for astro

--- nvim/lua/lspconfig/util.lua
+++ nvim/lua/lspconfig/util.lua
@@ -30,4 +30,16 @@ function Util.insert_package_json(root_files, field, fname)
   return Util.root_markers_with_field(root_files, { 'package.json', 'package.json5' }, field, fname)
 end
 
+function Util.get_typescript_server_path(root_dir)
+  local project_roots = vim.fs.find('node_modules', { path = root_dir, upward = true, limit = math.huge })
+  for _, project_root in ipairs(project_roots) do
+    local typescript_path = project_root .. '/typescript'
+    local stat = vim.loop.fs_stat(typescript_path)
+    if stat and stat.type == 'directory' then
+      return typescript_path .. '/lib'
+    end
+  end
+  return ''
+end
+
 return Util
