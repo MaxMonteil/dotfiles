984631a8e29bfd55e3d7830331976c62d527bb1f
Maximilien Monteil <maximilienmonteil@gmail.com>
2026-02-09 22:22:06 +0100

    feat(nvim): organize mini plugin file

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
   'nvim-mini/mini.nvim',
   version = '*',
   config = function()
+    -- Basic Modules --
     require('mini.icons').setup()
     require('mini.tabline').setup()
     require('mini.diff').setup()
@@ -9,24 +10,11 @@ return {
     require('mini.pairs').setup()
     require('mini.statusline').setup()
     require('mini.surround').setup()
+    require('mini.snippets').setup()
 
-    require('mini.files').setup({
-      windows = {
-        preview = true,
-        width_preview = 75,
-      },
-    })
-    vim.keymap.set('n', '<Leader>e', function()
-      if not MiniFiles.close() then MiniFiles.open(vim.api.nvim_buf_get_name(0)) end
-    end, { desc = 'Toggle file explorer.' })
-
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'MiniFilesWindowOpen',
-      callback = function()
-        vim.keymap.set('n', '<ESC>', MiniFiles.close, { desc = 'Close file explorer when open.' })
-      end,
-    })
-
+    -------------
+    -- Comment --
+    -------------
     require('mini.comment').setup({
       options = {
         custom_commentstring = function()
@@ -34,17 +22,51 @@ return {
         end,
       },
     })
+    -- Comment end --
 
+    ----------------
+    -- Completion --
+    ----------------
     require('mini.completion').setup({
       lsp_completion = {
         source_func = 'omnifunc',
         auto_setup = true,
       },
     })
+    -- Completion end --
 
-    require('mini.snippets').setup()
+    -----------
+    -- Files --
+    -----------
+    local mini_files = require('mini.files')
 
+    mini_files.setup({
+      windows = {
+        preview = true,
+        width_preview = 75,
+      },
+    })
+
+    vim.keymap.set('n', '<Leader>e', function()
+      if not mini_files.close() then mini_files.open(vim.api.nvim_buf_get_name(0)) end
+    end, { desc = 'Toggle file explorer.' })
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesWindowOpen',
+      callback = function(args)
+        vim.keymap.set('n', '<ESC>', mini_files.close, {
+          desc = 'Close file explorer.',
+          buffer = args.buf,
+        })
+      end,
+    })
+    -- Files end --
+
+    ------------
+    -- Keymap --
+    ------------
     local mini_keymap = require('mini.keymap')
+
     mini_keymap.setup()
 
     local map_multistep = mini_keymap.map_multistep
@@ -57,5 +79,6 @@ return {
 
     map_multistep('i', '<CR>', { 'pmenu_accept', 'minipairs_cr' })
     map_multistep('i', '<BS>', { 'minipairs_bs' })
+    -- Keymap end --
   end
 }
