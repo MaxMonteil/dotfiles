2c4af143b6c870cc7fa77ffffd9be184b1a128d7
Maximilien Monteil <maximilienmonteil@gmail.com>
2026-04-11 18:36:10 +0200

    feat(aliases): add gcm alias to switch to main branch

--- aliases.zsh
+++ aliases.zsh
@@ -16,6 +16,33 @@ alias vim=nvim
 alias gst="git status"
 alias gco="git checkout"
 
+## Get the default branch name from common branch names or fallback to remote HEAD
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  
+  local remote ref
+  
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return 0
+    fi
+  done
+  
+  # Fallback: try to get the default branch from remote HEAD symbolic refs
+  for remote in origin upstream; do
+    ref=$(command git rev-parse --abbrev-ref $remote/HEAD 2>/dev/null)
+    if [[ $ref == $remote/* ]]; then
+      echo ${ref#"$remote/"}; return 0
+    fi
+  done
+
+  # If no main branch was found, fall back to master but return error
+  echo master
+  return 1
+}
+alias gcm="git checkout $(git_main_branch)"
+
 # project shortcuts
 alias mylo="cd ~/Code/Mylo"
 alias myapp="cd ~/Code/Mylo/packages/app"
